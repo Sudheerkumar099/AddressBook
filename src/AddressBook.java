6438ae2cdfcd36c0fdd1fbbdea8d17946f0362a2
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-10 12:35:04 +0530

    create the variables  getters setters and tostring

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,7 @@
 public class AddressBook {
     public static void main(String[] args) {
-        
+//       Contact c= new Contact("sudheer","tatannagari","tirupati","tirupati","andhra",517501,456789,"tsudheerkumar09@gmail.com");
+//        System.out.println(c);
+
     }
 }
--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,47 @@
 public class Contact {
+//variables
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private String email;
+
+    //constructor
+    public Contact(String firstName, String lastName, String address, String city,
+                   String state, int zip, long phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters & Setters
+    public String getFirstName() { return firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public String getAddress() { return address; }
+
+    public String getCity() { return city; }
+
+    public String getState() { return state; }
+
+    public int getZip() { return zip; }
+
+    public long getPhoneNumber() { return phoneNumber; }
+
+    public String getEmail() { return email; }
+
+    @Override
+    public String toString() {
+        return
+               "First name: "+  firstName +" LastName: "+ lastName +" Address: "+ address +" City: "+ city +" Zip:"+zip+" Phone number: "+phoneNumber;
+    }
 }
